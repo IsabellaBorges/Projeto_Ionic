e3957dfd4721b7715ecbfe59d5013c4c0ad8dbfc
ISABELLA CRISTINA SOUSA BORGES <28094@ETECPAOF.LAN>
2019-05-20 22:31:28 -0300

    Iniciando

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+
+  public bemvindo:string="Bem Vindo ao Ionic 4!";
+  constructor (public navCtrl:NavController) {
+  }
+
+    public teste ():void{
+      alert('Funcionou')
+    }
+    ngOnInit(){
+      this.teste();
+     
+    }
+  }
+ 
+
+
+  
